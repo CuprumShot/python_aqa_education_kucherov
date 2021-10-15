30f9273a5dc82f341377265ce62ee1347ff980f1
Kir Kucherov <don.kerya@gmail.com>
2021-10-15 23:39:54 +0300

    hello world task was finished
    variables_types.py has been added
    
    docstrings.py has been added
    
    all commits has been squashed
    
    hello world task was finished
    
    hello world task was finished
    
    variables_types.py has been added
    
    docstrings.py has been added
    
    all commits has been squashed
    
    hello world task was finished
    
    Coffee Machine Project
    
    Coffee machine final

---
+++ basics/docstrings.py
@@ -0,0 +1,16 @@
+'''
+Here is an example of a multi-line docstring.
+'''
+
+
+def my_function():
+     """Do nothing, but document it.
+
+     No, really, it doesn't do anything.
+     """
+     pass
+
+     print(my_function.__doc__)
+
+if __name__ == '__main__':
+     my_function()
